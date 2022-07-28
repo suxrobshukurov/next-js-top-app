d4ec7a0e4bd76431f5a9cb566365102be647afc5
Suxrob Shukurov <sdead96@gmail.com>
2022-07-28 20:50:31 +0300

    added style for footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import cn from "classnames";
 import { FooterProps } from "./Footer.props";
 import styles from './Footer.module.css';
+import { format } from "date-fns";
 
-export const Footer = ({...props}: FooterProps): JSX.Element  =>{
+export const Footer = ({ className, ...props}: FooterProps): JSX.Element  =>{
   return(
-    <div {...props}>
-      Footer
-    </div>
+    <footer {...props} className={cn(className, styles.footer)}>
+      <div> OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены </div>
+      <a href="#" target='_blank'>Пользовательское соглашение</a>
+      <a href="#" target='_blank'>Политика конфиденциальности</a>
+    </footer>
   )
 }
